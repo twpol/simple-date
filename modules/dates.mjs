def7441469298fe99b377925cf8c6a9984735464
James Ross <git@james-ross.co.uk>
2023-06-01 20:10:32 +0100

    feat: Add styles and test modes

--- modules/dates.mjs
+++ modules/dates.mjs
@@ -1,6 +1,17 @@
+/**
+ * @type {(Date) => number}
+ */
 export function getIsoWeek(date) {
-	const temp = document.createElement("input");
-	temp.type = "week";
-	temp.valueAsDate = date;
-	return temp.value.split("W").pop();
+	const week1 = getWeekFromYear(date.getFullYear() - 1, date);
+	const week2 = getWeekFromYear(date.getFullYear() + 0, date);
+	const week3 = getWeekFromYear(date.getFullYear() + 1, date);
+	return week2 <= 0 ? week1 : week3 > 0 ? week3 : week2;
+}
+
+function getWeekFromYear(year, date) {
+	const jan1 = new Date(year, 0, 1);
+	const ordinal = 1 + Math.floor((date - +jan1) / (1000 * 60 * 60 * 24));
+	const day = date.getDay() || 7;
+	const week = Math.floor((ordinal - day + 10) / 7);
+	return week;
 }
--- pages/index.mjs
+++ pages/index.mjs
@@ -7,18 +7,40 @@ const QUARTERS = ["Q1", "Q2", "Q3", "Q4"];
 const MONTHS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 const DAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
+const url = new URLSearchParams(location.search);
+const test = url.get("test");
+let count = 0;
+
+function getDate() {
+	if (test) {
+		if (test.length === 10) {
+			return new Date(test);
+		}
+		return new Date(+new Date("2020-12-01") + count++ * 1000 * 60 * 60 * 24);
+	}
+	return new Date();
+}
+
 function update() {
-	const date = new Date();
+	const date = getDate();
 	const isoWeek = getIsoWeek(date);
 
 	e.date.calendar.quarter.textContent = QUARTERS[Math.floor(date.getMonth() / 3)];
+
 	e.date.calendar.month.textContent = MONTHS[date.getMonth()];
+	e.date.calendar.month.setAttribute("sub", date.getMonth() + 1);
+
 	e.date.calendar.day.textContent = date.getDate();
 
 	e.date.mumps.mump.textContent = `M${1 + Math.floor((isoWeek - 1) / 4)}`;
+
 	e.date.mumps.week.textContent = `W${isoWeek}`;
+	e.date.mumps.week.setAttribute("sub", `${1 + ((isoWeek - 1) % 4)}/4`);
+	e.date.mumps.week.setAttribute("tag", isoWeek % 4);
+
 	e.date.mumps.dow.textContent = DAYS[date.getDay()];
+	e.date.mumps.dow.setAttribute("tag", date.getDay());
 }
 
-setInterval(update, 10000);
+setInterval(update, 1000);
 update();
